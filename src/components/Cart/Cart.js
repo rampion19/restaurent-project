41c16936faf9e401b62387f01a15d8e660b52d3b
rampion19 <princegupta.pg19@gmail.com>
2023-02-10 19:14:50 +0530

    Added visible state and handler for cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,14 +6,15 @@ const Cart = props => {
     const cartItems = <ul>{[{ id: 'c1', name: 'Sushi', amount: 2, price: 12 }].map(item =>
         <li>{item.name}</li>)}</ul>
     return (
-        <Modal >
+        <Modal onClose = {props.onClose}>
             {cartItems}
             <div className={classes.total} >
                 <span>Total Amount</span>
                 <span>30</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes['button--alt']}>Close</button>
+                <button className={classes['button--alt']}
+                    onClick={props.onClose}>Close</button>
                 <button className={classes.button}>order</button>
             </div>
         </Modal>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,26 +1,25 @@
 import { Fragment } from 'react'
 import classes from './Modal.module.css'
-import  ReactDOM  from 'react-dom'
+import ReactDOM from 'react-dom'
 
 const Backdrop = props => {
 
-    return <div className= {classes.backrop} />
-
+    return <div className={classes.backrop} onClick = {props.onClose}/>
 }
 
 const ModalOverlays = props => {
 
-    return <div className= {classes.modal} >
-    <div className= {classes.content}>{props.children}</div>
+    return <div className={classes.modal} >
+        <div className={classes.content}>{props.children}</div>
     </div>
 }
 
 const portalElements = document.getElementById("overlays")
 const Modal = props => {
-return <Fragment>
-    {ReactDOM.createPortal(<Backdrop />,portalElements)}
-    {ReactDOM.createPortal(<ModalOverlays>{props.children}</ModalOverlays>, portalElements)}
-</Fragment>
+    return <Fragment>
+        {ReactDOM.createPortal(<Backdrop onClose = {props.onClose}/>, portalElements)}
+        {ReactDOM.createPortal(<ModalOverlays>{props.children}</ModalOverlays>, portalElements)}
+    </Fragment>
 }
 
 export default Modal;
